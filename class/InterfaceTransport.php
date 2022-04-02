c129087d202738ac4de7fd5bc17869ec7d3dc062
ThroughEternity <eternity1666@gmail.com>
2022-04-02 19:31:22 +0400

    add interface

---
+++ class/InterfaceTransport.php
@@ -0,0 +1,13 @@
+<?
+
+interface InterfaceTransport{
+    public function getName();
+    public function getColor();
+    public function getHP();
+    public function getMaxSpeed();
+    public function turnOn();
+    public function forward();
+    public function reverse();
+    public function specAction();
+    public function showInterior();
+}
